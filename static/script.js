08be9279537fd331484e1fb26910ef42034faa12
Eden <edgib102@gmail.com>
2025-08-16 01:22:12 +1000

    og

--- static/script.js
+++ static/script.js
@@ -7,39 +7,37 @@
 
  
  
-window.onload = async () => {
+ window.onload = async () => {
 
-  const devices = await navigator.mediaDevices.enumerateDevices();
-
-  // Filter only video inputs that are not IR
-  const videoDevices = devices.filter(d => 
-    d.kind === 'videoinput' && 
-    !d.label.toLowerCase().includes('ir')
-  );
-
-  if (videoDevices.length < 2) {
-    throw new Error(`Need at least 2 RGB cameras, but found ${videoDevices.length}`);
-  }
 
   
-  const [video1, video2] = await initCameras();
+    const [video1, video2] = await initCameras();
 
-  const pose1 = new Pose({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}` });
-  const pose2 = new Pose({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}` });
+    const pose1 = new Pose({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}` });
+    const pose2 = new Pose({ locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}` });
 
-  pose1.setOptions({ modelComplexity: 2, smoothLandmarks: true, minDetectionConfidence: 0.75, minTrackingConfidence: 0.75 });
-  pose2.setOptions({ modelComplexity: 2, smoothLandmarks: true, minDetectionConfidence: 0.75, minTrackingConfidence: 0.75 });
+    pose1.setOptions({ modelComplexity: 2, smoothLandmarks: true, minDetectionConfidence: 0.75, minTrackingConfidence: 0.75 });
+    pose2.setOptions({ modelComplexity: 2, smoothLandmarks: true, minDetectionConfidence: 0.75, minTrackingConfidence: 0.75 });
 
-  pose1.onResults(results => renderResults(aresults, 'output1'));
-  pose2.onResults(results => renderResults(results, 'output2'));
+    pose1.onResults(results => renderResults(aresults, 'output1'));
+    pose2.onResults(results => renderResults(results, 'output2'));
 
-  detectFrame(video1, pose1);
-  detectFrame(video2, pose2);
+    detectFrame(video1, pose1);
+    detectFrame(video2, pose2);
 };
 
     async function initCameras() {
       const devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices.filter(d => d.kind === 'videoinput');
+
+      // Filter only video inputs that are not IR
+      const videoDevices = devices.filter(d => 
+        d.kind === 'videoinput' && 
+        !d.label.toLowerCase().includes('ir')
+      );
+
+      if (videoDevices.length < 2) {
+        throw new Error(`Need at least 2 RGB cameras, but found ${videoDevices.length}`);
+      }
 
       const video1 = document.getElementById('webcam1');
       const video2 = document.getElementById('webcam2');
